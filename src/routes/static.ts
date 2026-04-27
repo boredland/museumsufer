dcf13dd2e596047e1c853f5042e837c94c39e0c6
Jonas Strasel <info@jonas-strassel.de>
2026-04-28 01:15:28 +0200

    Use todayIso() (Berlin time) for sitemap lastmod instead of UTC

--- src/routes/static.ts
+++ src/routes/static.ts
@@ -1,4 +1,5 @@
 import { Hono } from "hono";
+import { todayIso } from "../date";
 import { SERVICE_WORKER_JS } from "../service-worker";
 import type { Env } from "../types";
 
@@ -85,7 +86,7 @@ app.get("/robots.txt", (c) =>
 );
 
 app.get("/sitemap.xml", (c) => {
-  const today = new Date().toISOString().slice(0, 10);
+  const today = todayIso();
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url><loc>https://museumsufer.app/</loc><lastmod>${today}</lastmod><changefreq>daily</changefreq><priority>1.0</priority></url>
